22fcc2882b602c5d7721243f76e26a8b89c66fb9
Daniel Mrva <mrva.paints@gmail.com>
2022-03-08 20:59:35 -0700

    working html output, needs formatting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Employee = require("./lib/Employee")
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// const { exit } = require("process");
+
 
 //arrays containing prompt questions
 const mgrPrompt = [
@@ -104,68 +104,100 @@ const addMember = [
     
 ]
 
-const baseHtml = 
-`<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    <title>${teamName}</title>
-  </head>
-  <body>
-    <h1>${teamName}</h1>
-        <main class="container">
-            <section class="row team-container">
-                ${compiledCards}
-            </section>
-        </main>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  </body>
-</html>
-`
-let compiledCards =+ newCard;
-
-
-switch (employed.getRole()) {
-    case "Manager":
-        let newCard = 
-            `
-            <div class="card col" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">{nameX}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-            </div>
-            </div>
-            `
-        break;
-
-    default:
-        break;
-}
-
-let newCard = 
-`
-<div class="card col" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">{nameX}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-`
-let team = [];
 let mngArray = [];
 let engArray = [];
 let intArray = [];
+let team = [];
+
+async function buildCards(team) {
+    let compiledCards = '';
+    team.forEach(teamMember => {
+        switch (teamMember.getRole()) {
+            case "Manager":{ 
+                let newCard = 
+            `   <div class="card col" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${teamMember.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${teamMember.getRole()}</h6>
+                    <p class="card-text">${teamMember.name}'s id is ${teamMember.id}, and their office number is ${teamMember.officeNumber}</p>
+                    <a href="mailto:${teamMember.email}" class="card-link">Email</a>
+                </div>
+                </div>
+            `   
+                compiledCards += newCard;
+                break;
+            }
+            case "Engineer":{ 
+                let newCard = 
+            `   <div class="card col" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${teamMember.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${teamMember.getRole()}</h6>
+                    <p class="card-text">${teamMember.name}'s id is ${teamMember.id}</p>
+                    <a href="mailto:${teamMember.email}" class="card-link">Email</a>
+                    <a href="https://github.com/${teamMember.github}" class="card-link">Github</a>
+                </div>
+                </div>
+            `   
+                compiledCards += newCard;
+                break;
+            }
+            case "Intern":{ 
+                let newCard = 
+            `   <div class="card col" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${teamMember.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${teamMember.getRole()}</h6>
+                    <p class="card-text">${teamMember.name}'s id is ${teamMember.id}, they are a part of ${teamMember.school}'s coding internship program.</p>
+                    <a href="mailto:${teamMember.email}" class="card-link">Email</a>
+                </div>
+                </div>
+            `   
+                compiledCards += newCard;
+                break;
+            }
+        }
+    });
+    console.log(compiledCards);
+    return compiledCards;
+} 
+
+async function htmlCompiler(compiledCards) {
+    return baseHtml = 
+    `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <title>Tech Team!</title>
+      </head>
+      <body>
+        <h1>Tech Team!</h1>
+            <main class="container">
+                <section class="row team-container">
+                    ${compiledCards}
+                </section>
+            </main>
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+      </body>
+    </html>
+    `
+}
+
+async function fileHandler() {
+    fs.writeFileSync("./dist/index.html", baseHtml, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("HTML file sucessfully created! YAY!")
+        }
+    })
+}
+
 
 async function recruitment() {
     
@@ -187,9 +219,6 @@ async function recruitment() {
             await recruitment ()   
             break};
         case "No Additional Members":
-            console.log(engArray);
-            console.log(intArray);
-            
             break;
     }
     return
@@ -199,14 +228,27 @@ async function teamBuilder() {
 
     let manager = await inquirer.prompt(mgrPrompt);
     if (manager) {
-        let teamName = manager.teamName;
+        // let teamName = manager.teamName;
         let teamMgr = new Manager(manager.mgrName, manager.mgrID, manager.mgrEmail, manager.mgrOffice) 
         console.log(teamMgr);
-        team.push(teamName);
+        // team.push(teamName);
         mngArray.push(teamMgr);
     }
 
     await recruitment();
+
+    team = [...mngArray, ...engArray, ...intArray]
+    
+
+    let cards = await buildCards(team);
+    await htmlCompiler(cards);
+    await fileHandler(baseHtml, cards);
+    // console.log(team);
+    // console.log(team[0].name)
+    // console.log(team[0].getRole())
+    // console.log(team[1].getRole())
+    // console.log(compiledCards);
+
 
 }
 
